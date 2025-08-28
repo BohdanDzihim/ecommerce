0b23608ac6308ba815ca989d02e33305c458131d
BohdanDzihim <bohdan.dzihim@gmail.com>
2025-08-28 11:57:38 +0200

    Remove background color from login and registration page containers for consistency

--- frontend/src/app/users/login/page.tsx
+++ frontend/src/app/users/login/page.tsx
@@ -35,7 +35,7 @@ const Login = () => {
   };
 
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4'>
+    <div className='min-h-screen flex items-center justify-center px-4'>
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
         <div className='text-3xl font-bold text-center mb-6'>Login</div>
 
--- frontend/src/app/users/register/page.tsx
+++ frontend/src/app/users/register/page.tsx
@@ -49,7 +49,7 @@ const RegistrationPage = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+    <div className="min-h-screen flex items-center justify-center px-4">
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6">Create an Account</h2>
 
